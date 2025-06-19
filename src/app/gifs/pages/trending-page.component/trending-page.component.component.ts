8599153a321f1fef36665d489b95bf6e7221a162
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-06-19 10:54:10 -0600

    Se restructura el codigo del html y vuelve a estar como antes

--- src/app/gifs/pages/trending-page.component/trending-page.component.component.ts
+++ src/app/gifs/pages/trending-page.component/trending-page.component.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifListItemComponent } from "../../components/gif-list-item/gif-list-item.component";
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'app-trending-page.component',
   standalone: true,
-  imports: [],
+  imports: [GifListItemComponent, GifListComponent],
   templateUrl: './trending-page.component.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
