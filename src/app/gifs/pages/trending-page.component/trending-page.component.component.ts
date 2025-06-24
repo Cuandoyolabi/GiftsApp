6d0501b03ff36059ea3c6029329f2257a8705249
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-06-24 12:54:37 -0600

    Se guarda el proyecto, se intenta implementar un scroll infinito

--- src/app/gifs/pages/trending-page.component/trending-page.component.component.ts
+++ src/app/gifs/pages/trending-page.component/trending-page.component.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifListItemComponent } from "../../components/gif-list-item/gif-list-item.component";
 import { GifListComponent } from "../../components/gif-list/gif-list.component";
 import { GifService } from '../../services/gifs.service';
@@ -7,12 +7,25 @@ import { GifService } from '../../services/gifs.service';
 @Component({
   selector: 'app-trending-page.component',
   standalone: true,
-  imports: [GifListItemComponent, GifListComponent],
+  //imports: [GifListItemComponent, GifListComponent],
   templateUrl: './trending-page.component.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class TrendingPageComponentComponent {
   gifService = inject(GifService);
+
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  onScroll(event : Event){
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if( !scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clientHeight = scrollDiv.clientHeight;
+    const scrollHeight = scrollDiv.scrollHeight;
+
+    console.log({scrollTop, clientHeight});
+  }
 
 }
 
