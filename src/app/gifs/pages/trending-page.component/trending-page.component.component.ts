31a29014014c5d1cc18679364cb08bbc28666373
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-06-24 13:34:39 -0600

    Se crea otro servicio, donde se implementa el guardado de la cantidad de pixeles de la pagina Top para regresar al usuario en la misma posicion donde estaba

--- src/app/gifs/pages/trending-page.component/trending-page.component.component.ts
+++ src/app/gifs/pages/trending-page.component/trending-page.component.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
-import { GifListItemComponent } from "../../components/gif-list-item/gif-list-item.component";
-import { GifListComponent } from "../../components/gif-list/gif-list.component";
-import { GifService } from '../../services/gifs.service';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 
+import { GifService } from '../../services/gifs.service';
+import { ScrollStateService } from './../../shared/services/scroll-state.service';
 
 @Component({
   selector: 'app-trending-page.component',
@@ -11,10 +10,18 @@ import { GifService } from '../../services/gifs.service';
   templateUrl: './trending-page.component.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class TrendingPageComponentComponent {
+export default class TrendingPageComponentComponent  implements AfterViewInit {
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
 
   scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if( !scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+  }
 
   onScroll(event : Event){
     const scrollDiv = this.scrollDivRef()?.nativeElement;
@@ -25,7 +32,12 @@ export default class TrendingPageComponentComponent {
     const scrollHeight = scrollDiv.scrollHeight;
 
     console.log({scrollTop, clientHeight});
-  }
+    const isABottom = scrollTop + clientHeight + 300 >= scrollHeight;
+    this.scrollStateService.trendingScrollState.set(scrollTop);
 
+    if (isABottom){
+      this.gifService.loadTrendingGifs();
+    }
+  }
 }
 
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -23,7 +23,9 @@ export class GifService {
   private http = inject(HttpClient);
 
   trendingGifs = signal<Gif[]>([])
-  trendingGifsLoading = signal(true);
+  trendingGifsLoading = signal(false);
+  private trendingPage = signal(0);
+
 
   //Arreglos de tres elementos
   trendingGifGroup = computed<Gif[][]>(() => {
@@ -47,7 +49,6 @@ export class GifService {
   }
 
   saveGifsToLocalStorage = effect(() => {
-
     const historyString = JSON.stringify(this.searchHistory());
     localStorage.setItem('gifs', historyString);
 
@@ -55,14 +56,22 @@ export class GifService {
 
 
   loadTrendingGifs() {
+
+    if(this.trendingGifsLoading()) return;
+
+    this.trendingGifsLoading.set(true);
+
     this.http.get<GiphyResponse>(`${ environment.giphyUrl }/gifs/trending`, {
       params: {
         api_key: environment.giphyApiKey,
         limit: 21,
+        offset: this.trendingPage() * 20,
       }
     }).subscribe( (resp) => {
       const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
-      this.trendingGifs.set(gifs);
+      this.trendingGifs.update( currentGifs => [...currentGifs, ...gifs]);
+      this.trendingPage.update( page => page + 1)
+
       this.trendingGifsLoading.set(false);
       console.log(gifs)
     }  )
