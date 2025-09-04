455e184d2e5ab8b0cc780bd65281b7ac0041309f
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-09-04 13:27:55 -0600

    "Modificando configuración de TailwindCSS"

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// filepath: c:\Users\Adami\OneDrive\Desktop\Proyectos JavaScript\First Proyect\GiftsApp\tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
